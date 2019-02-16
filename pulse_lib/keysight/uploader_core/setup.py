b93662fbcdcafdee931e1dfd563aa0fa9c8a0210
Stephan Philips <s.g.j.philips@tudelft.nl>
2019-02-16 14:41:34 +0100

    fix merge

--- pulse_lib/keysight/uploader_core/setup.py
+++ pulse_lib/keysight/uploader_core/setup.py
@@ -3,20 +3,6 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
-<<<<<<< HEAD
-extensions = [
-    Extension("uploader", 
-    	sources = ["uploader.pyx","mem_ctrl.cpp", "keysight_awg_post_processing_and_upload.cpp"],
-        include_dirs=[numpy.get_include(),"/usr/local/include/Keysight/SD1", ],
-        libraries=["SD1core", "SD1pxi", "gomp", "boost_python37"],
-        library_dirs=["/usr/local/lib/Keysight/SD1/"],
-        language='c++',
-		extra_compile_args=['-fopenmp'],
-        )
-=======
-import os
->>>>>>> f42340300141c5c8523f9a32feb242b9e51a8dc1
-
 if os.name == 'nt':
 	extensions = [
 		Extension("uploader", 
