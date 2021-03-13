1056f2ab1036de3746a98b8a1b57960e40f31467
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2021-03-13 15:55:59 +0100

    Added test with mocked M3202A

---
+++ pulse_lib/tests/hw_schedule_mock.py
@@ -0,0 +1,35 @@
+import logging
+
+class HardwareScheduleMock:
+    def __init__(self):
+        self.loaded = False
+
+    def compile(self, sequencer_hardware):
+        pass
+
+    def load(self):
+        logging.info('load schedule')
+        self.loaded = True
+
+    def is_loaded(self):
+        return self.loaded
+
+    def unload(self):
+        logging.info('unload schedule')
+        self.loaded = False
+
+    def set_schedule_parameters(self, **kwargs):
+        logging.info(f'set parameters {kwargs}')
+
+    def set_configuration(self, *args, **kwargs):
+        logging.info(f'set configuration {args} {kwargs}')
+
+    def start(self, waveform_duration, n_repetitions, sequence_params):
+        logging.info(f'start {n_repetitions}*{waveform_duration} {sequence_params}')
+
+    def is_running(self):
+        return False
+
+    def close(self):
+        logging.info(f'close()')
+
