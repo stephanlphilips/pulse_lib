c1bac2d017930bda94f448fd035e854049b858ce
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2021-05-31 14:53:23 +0200

    cleanup

--- pulse_lib/segments/utility/rounding.py
+++ pulse_lib/segments/utility/rounding.py
@@ -5,6 +5,3 @@ def iround(value):
     Fast implementation of round which uses half-up, i.s.o. half-even rounding.
     '''
     return np.int_(value + 0.5)
-
-#def get_effective_point_number(time, time_step):
-#    return int(time/time_step + 0.5)
