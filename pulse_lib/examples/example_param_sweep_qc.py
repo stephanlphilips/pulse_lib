e880ee6163c093ca10042afbe2e1212fb424e2d2
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2022-03-16 16:19:32 +0100

    Added example with parameter sweep and qcodes DataSet

---
+++ pulse_lib/examples/example_param_sweep_qc.py
@@ -0,0 +1,75 @@
+from pulse_lib.tests.hw_schedule_mock import HardwareScheduleMock
+import pulse_lib.segments.utility.looping as lp
+
+from qcodes import Station
+from qcodes.data.data_set import DataSet
+from qcodes.data.io import DiskIO
+from qcodes.measure import Measure
+from qcodes.loops import Loop
+from qcodes.actions import Task
+
+from configuration.small import init_hardware, init_pulselib
+#from utils.plot import plot_awgs
+from utils.dummy_param import DummyParam
+
+def upload_play(seq):
+    seq.upload()
+    seq.play()
+    # plot_awgs(awgs)
+
+# TODO: add as qc_utility to pulselib.
+def qc_run(name, seq, *params):
+    loop = None
+    for sp in seq.params:
+        if loop is None:
+            loop = Loop(sp[sp.values])
+        else:
+            loop = loop.loop((sp[sp.values]))
+
+    play_task = Task(upload_play, seq)
+
+    if loop is not None:
+        m = loop.each(play_task, *params)
+    else:
+        m = Measure(play_task, *params)
+
+    ds = m.run(loc_record={'name':name})
+    return ds
+
+
+path = 'C:/Projects/test/data'
+
+io = DiskIO(path)
+DataSet.default_io = io
+
+station = Station()
+
+# create "AWG1"
+awgs = init_hardware()
+
+# create channels P1, P2
+p = init_pulselib(awgs)
+
+v_param = lp.linspace(0, 200, 5, axis=0, unit = "mV", name = "vPulse")
+t_wait = lp.linspace(20, 100, 3, axis=1, unit = "mV", name = "t_wait")
+
+
+seg1 = p.mk_segment()
+seg2 = p.mk_segment()
+
+seg1.P1.add_ramp_ss(0, 100, 0, v_param)
+seg1.P1.add_block(100, 200, v_param)
+
+seg2.P2.add_block(0, 100, 200)
+seg2.P2.wait(t_wait)
+seg2.reset_time()
+seg2.add_HVI_marker('dig_trigger_1', t_off=50)
+seg2.P1.add_block(0, 100, v_param)
+
+# create sequence
+seq = p.mk_sequence([seg1,seg2])
+seq.set_hw_schedule(HardwareScheduleMock())
+
+dig_param = DummyParam('dummy', 200, 10)
+
+qc_run('SweepDemo', seq, dig_param)
