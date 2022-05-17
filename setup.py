41e9e35912a654c3e3fe7fa447ac9e0cc8ba8b6a
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2022-05-17 12:08:11 +0200

    Bump version: 1.3.5 → 1.4.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ print('packages: %s' % packages)
 
 
 setup(name="pulse_lib",
-	version="1.3.5",
+	version="1.4.0",
 	packages = find_packages(),
     python_requires=">=3.7",
     install_requires=['si_prefix', 'qcodes>=0.27.0'],
