349502a961e8bc43b32ce2f4f265e5f0ffbb2808
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2023-01-19 11:07:27 +0100

    bump version 1.6.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ print('packages: %s' % packages)
 
 
 setup(name="pulse_lib",
-	version="1.6.2",
+	version="1.6.3",
 	packages = find_packages(),
     python_requires=">=3.7",
     install_requires=[
