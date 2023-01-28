372dacb74d0649be6fd67a9e0dff1bf1359008c1
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2023-01-28 16:21:13 +0100

    Fixed rounding of negative numbers

--- pulse_lib/segments/utility/rounding.py
+++ pulse_lib/segments/utility/rounding.py
@@ -4,4 +4,4 @@ def iround(value):
     '''
     Fast implementation of round which uses half-up, i.s.o. half-even rounding.
     '''
-    return np.int_(value + 0.5)
+    return np.floor(value + 0.5).astype(int)
