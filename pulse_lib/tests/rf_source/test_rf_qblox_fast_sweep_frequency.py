f0f8fedca51ddd5a1eb395f88892e321fa1d1f46
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2023-07-28 16:22:45 +0200

    Test sweep of resonator drive amplitude

--- pulse_lib/tests/rf_source/test_rf_qblox_fast_sweep_frequency.py
+++ pulse_lib/tests/rf_source/test_rf_qblox_fast_sweep_frequency.py
@@ -59,18 +59,17 @@ def test_freq():
 
     return ds
 
-# TODO: Fix! Amplitude sweep does not work.
-#def test_ampl():
-#    pulse = context.init_pulselib(n_gates=2, n_sensors=2, rf_sources=True)
-#
-#    rf_amplitude = RfAmplitudeParameter(pulse, 'SD2')
-#    fast_scan_param = fast_scan1D_param(pulse, 'P1', 50.0, 21, 2_000, iq_mode='I+Q')
-#    ds = do1D(rf_amplitude, 20.0, 200.0, 10, 0.0, fast_scan_param, name='amplitude_sweep', reset_param=True).run()
-#
-#    return ds
+def test_ampl():
+    pulse = context.init_pulselib(n_gates=2, n_sensors=2, rf_sources=True)
+
+    rf_amplitude = RfAmplitudeParameter(pulse, 'SD2')
+    fast_scan_param = fast_scan1D_param(pulse, 'P1', 50.0, 21, 2_000, iq_mode='I+Q', reload_seq=True)
+    ds = do1D(rf_amplitude, 20.0, 200.0, 10, 0.0, fast_scan_param, name='amplitude_sweep', reset_param=True).run()
+
+    return ds
 
 #%%
 if __name__ == '__main__':
     context.init_coretools()
     ds1 = test_freq()
-#    ds2 = test_ampl()
+    ds2 = test_ampl()
