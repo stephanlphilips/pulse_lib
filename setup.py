b3f0a5b237525ca18ec7c6b616788679656a1b16
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2023-10-10 09:06:25 +0200

    Bump to 1.7.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ print('packages: %s' % packages)
 
 
 setup(name="pulse_lib",
-	version="1.7.3",
+	version="1.7.4",
 	packages = find_packages(),
     python_requires=">=3.7",
     install_requires=[
