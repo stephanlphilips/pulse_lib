Fix units in measurement parameter

pulse_lib/acquisition/iq_modes.py
@@ -3,17 +3,16 @@ import numpy as np
 def iq_mode2func(iq_mode):
     '''
     Returns:
-        func[np.array]->np.array, or
-        list[Tuple[str, func[np.array]->np.array]]
+        list[Tuple[str, func[np.array]->np.array], str]
     '''
     func_map = {
-        'Complex': lambda x:x,
-        'I': np.real,
-        'Q': np.imag,
-        'amplitude': np.abs,
-        'phase': np.angle,
-        'I+Q': [('_I', np.real), ('_Q', np.imag)],
-        'amplitude+phase': [('_amp', np.abs), ('_phase', np.angle)],
+        'Complex': [('', lambda x:x, 'mV')],
+        'I': [('', np.real, 'mV')],
+        'Q': [('', np.imag, 'mV')],
+        'amplitude': [('', np.abs, 'mV')],
+        'phase': [('', np.angle, 'rad')],
+        'I+Q': [('_I', np.real, 'mV'), ('_Q', np.imag, 'mV')],
+        'amplitude+phase': [('_amp', np.abs, 'mV'), ('_phase', np.angle, 'rad')],
         }
     try:
         return func_map[iq_mode]